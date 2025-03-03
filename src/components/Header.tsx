9d29424872145c4c69d257f7478d656305e8d25f
Arnold Jizdan <ostbrok@gmail.com>
2025-03-03 20:44:29 +0300

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,16 @@
 "use client";
 
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
+import { User } from "lucide-react";
 
 export default function Header() {
-  const [open, setOpen] = useState(false);
-
   return (
     <header className="flex justify-between items-center p-4 bg-white shadow-md">
+      <div className="mr-2"></div>
       <div className="text-xl font-bold">Tynetra</div>
-
-      {/* Бургер-меню */}
-      <Sheet open={open} onOpenChange={setOpen}>
-        <SheetTrigger asChild>
-          <Button variant="ghost" size="icon" className="po">
-            <Menu className="w-6 h-6" />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="right" className="w-64">
-          <nav className="flex flex-col space-y-4 p-4">
-            <a href="#" className="text-lg">
-              Главная
-            </a>
-            <a href="#" className="text-lg">
-              О нас
-            </a>
-            <a href="#" className="text-lg">
-              Контакты
-            </a>
-          </nav>
-        </SheetContent>
-      </Sheet>
+      <Button size="icon">
+        <User />
+      </Button>
     </header>
   );
 }
